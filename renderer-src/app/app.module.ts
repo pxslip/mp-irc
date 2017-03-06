4f11e097bc282cda2b0b9a9b967d474188443a8c
Will Kruse <wskruse@gmail.com>
2017-03-06 14:40:40 -0500

    Start adding the routing components

--- renderer-src/app/app.module.ts
+++ renderer-src/app/app.module.ts
@@ -2,14 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app/app.component';
+import { ServerListComponent } from './server-list/server-list.component';
+
+const appRoutes = [
+  {
+      path: 'chat/:channel',
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerListComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
     HttpModule
